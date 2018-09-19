93e4576cf04b55f99a45718150ee8e4b7d902c6a
Zer0-M <mjamil90@stuy.edu>
2018-09-19 01:51:09 -0400

    finished work03

--- work03.c
+++ work03.c
@@ -2,11 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-int generate_rand(){
-  srand(time(NULL));
-  return rand();
-}
 
 int main(){
-  printf("%d\n",generate_rand());
+  srand(time(NULL));
+  int ar[10];
+  for(int i=0;i<(int)(sizeof(ar)/4);i++){
+    int j=rand();
+    ar[i]=j;
+  }
+  ar[9]=0;
+  printf("First Array:\n");
+  for(int i=0;i<(int)(sizeof(ar)/4);i++){
+    printf("element %d: %d\n",i,ar[i]);
+  }
+  int a[10];
+  int *ap=&a;
+  int *arp=&ar;
+  for(int i=9;i>=0;i--){
+    *(ap+9-i)=*(arp+i);
+  }
+  printf("Second Array:\n");
+  for(int i=0;i<(int)(sizeof(a)/4);i++){
+    printf("element %d: %d\n",i,*(ap+i));
+  }
 }
